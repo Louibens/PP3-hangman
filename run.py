28b9a6078e67fd530c76e795a773507d928537da
Louise Benson <louise.benson@gmail.com>
2023-04-04 10:14:53 +0000

    Move possible word options to hangman google worksheet

--- run.py
+++ run.py
@@ -10,10 +10,18 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('hangman-leaderboard')
+SHEET = GSPREAD_CLIENT.open('hangman')
 
 leaderboard = SHEET.worksheet('leaderboard')
+words = SHEET.worksheet('words')
+
+word_options = words.get_all_values()
+
+#options = word_options.split() # Converts string of words into a list of strings 
+print(word_options)
 
 data = leaderboard.get_all_values()
+
+
 
 print(data)
--- words.py
+++
@@ -1,3 +0,0 @@
-words = 'Module Package Library Keyword	Boolean	Integer	Decimal	Function	String	Method	Object	Iterable	Tuple	Stringy	Complex	Lambda	Mutable	Syntax	Console	Iterator	Unicode	Element	Listcom	Docstring	Operator	Syntaxer	Property	Variable	Booleany	Equivale	Property	Function	Instance	Expressi	Literals	Terminal	Function	Directory	Argument	Overflow	Exception	Whitespace	Callbacks	Generator	Attribute	Coverage	Internals	Behavior	Sequence	Overload'
-
-options = words.split() # Converts string of words into a list of strings 

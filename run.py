b92cb257ca22c6a9de50fb4b1c85feb430b9fe0d
Louise Benson <louise.benson@gmail.com>
2023-04-07 14:37:12 +0000

    Add user input for hangman game and lives and points tracking

--- run.py
+++ run.py
@@ -22,19 +22,63 @@ word_options = words.get_all_values() #returns list of lists
 
 data = leaderboard.get_all_values()
 
-print(data)
-
 
 def get_word(word_options):
-	'''
-    Get word list from words worksheet
-    convert to a single list 
-    use Random to pick a word from the list
     '''
-    for word in word_options:
-        single_list = [item for sublist in word_options for item in sublist]
-        word = random.choice(single_list)
+    Create a single list from words worksheet
+    Use Random to select one word from single list
+    '''
+    single_list = [item for sublist in word_options for item in sublist]
+    word = random.choice(single_list)
 
     return word.upper()
 
 
+def hangman():
+    '''
+    use get_word function to select random word
+
+    '''
+    selected_word = get_word(word_options) # use get_word function to randomly select a word from the words worksheet
+    word_letters = set(selected_word) # identify letters in the word
+    alphabet = set(string.ascii_uppercase) 
+    guessed_letters = set()
+
+    lives = 7
+    points = 0
+
+    while len(word_letters) > 0 and lives > 0:
+
+        print('You have used these letters: ', ' '.join(guessed_letters))
+        print('You have', lives, 'lives left')
+
+        word_list = [letter if letter in guessed_letters else '_' for letter in selected_word]
+        print('Current word: ', ' '.join(word_list))
+
+        '''
+        Get user input
+        '''
+        user_guess = input('Pick a letter: ').upper()
+
+        if user_guess in alphabet - guessed_letters:
+            guessed_letters.add(user_guess)
+            if user_guess in word_letters:
+                word_letters.remove(user_guess)
+                points += 50
+
+            else:
+                lives = lives - 1
+                print('That letter is not in the word.')
+
+        elif user_guess in guessed_letters:
+            print('You have already picked that letter. Please try again.')
+
+        else:
+            print('Invalid choice. Please choose a letter of the alphabet')
+
+    if lives == 0:
+        print('You just died. The word was', selected_word)
+    else:
+        print('Congratulations! You guessed the word was ', selected_word, 'You scored', points, 'points!')
+
+hangman()

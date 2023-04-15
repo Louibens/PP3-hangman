e605634bca798e98364ab8282ff3e24d2746c3ef
Louise Benson <louise.benson@gmail.com>
2023-04-15 16:09:01 +0000

    Address items listed in problems tab

--- run.py
+++ run.py
@@ -17,12 +17,14 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman')
+# returns list of lists
+WORD_OPTIONS = SHEET.worksheet('words').get_all_values()
 
-WORD_OPTIONS = SHEET.worksheet('words').get_all_values() #returns list of lists
 
 def type(text):
     '''
@@ -57,88 +59,94 @@ def update_leaderboard():
 def display_leaderboard():
     """
     Displays the top 10 scores
-    Filters to only show users top score so same user is not displayed multiple times
+    Filters to only show users top score
+    so same user is not displayed multiple times
     """
     table = PrettyTable()
     table.field_names = SHEET.worksheet('leaderboard').row_values(1)
     data = SHEET.worksheet('leaderboard').get_all_values()
-    sorted_data = sorted(data[1:], key=lambda x: int(x[1]), reverse=True) # sorts worksheet data by second column values
+    # sorts worksheet data by second column values
+    sorted_data = sorted(data[1:], key=lambda x: int(x[1]), reverse=True)
     top_ten = sorted_data[:10]
     table.add_rows(top_ten)
     print(table)
 
-    while True:        
+    while True:
         go_back = input(Fore.WHITE + 'Please hit B to go back \n').upper()
         if go_back == 'B':
             game_menu()
         else:
-            print(f'{Fore.RED} That is not a valid option. Please try again.\n')  
+            print(Fore.RED + 'That is not a valid option. Please try again.\n')
+
 
 def welcome_msg():
     '''
     welcome message and username input
     '''
     global USERNAME
-    type(f'T H A N K   Y O U   F O R   V I S I T I N G   M Y   G A M E   O F\t\n') 
+    type('T H A N K   Y O U   F O R   V I S I T I N G   M Y   G A M E   O F\n')
     print(LOGO)
-    type(f"E N T E R   Y O U R   N A M E   T O   C O N T I N U E"'\n')
+    type("E N T E R   Y O U R   N A M E   T O   C O N T I N U E"'\n')
     print("\n")
     while True:
-            USERNAME = input(Fore.WHITE + 'Enter a username: \n').capitalize()
-            if len(USERNAME) == 0:
-                print(f"{Fore.RED}Please enter a valid username to continue!")
-            else:
-                break
+        USERNAME = input(Fore.WHITE + 'Enter a username: \n').capitalize()
+        if len(USERNAME) == 0:
+            print(f"{Fore.RED}Please enter a valid username to continue!")
+        else:
+            break
         
 
 def game_menu():
     '''
     present user with menu options
     '''
-    print( f"""{Fore.WHITE}
+    print(f"""{Fore.WHITE}
         A - PLAY HANGMAN
         B - LEADERBOARD
         C - INSTRUCTIONS
         D - EXIT GAME
         """)
-    while True:        
-        user_choice = input(Fore.WHITE + 'Please choose an option from the list above: \n').upper()
+    while True:
+        user_choice = input(Fore.WHITE + 'Please choose an option from the '
+                            'list above: \n').upper()
         if user_choice == 'A':
             hangman()
         elif user_choice == 'B':
             display_leaderboard()
         elif user_choice == 'C':
-            display_instructions()  
+            display_instructions()
         elif user_choice == 'D':
-            print(f'Thanks for playing')
+            print(f'Thanks for playing {USERNAME}')
             exit()
         else:
-            print(f'{Fore.RED} That is not a valid option. Please try again.\n')  
+            print(Fore.RED + 'That is not a valid option. Please try again.\n')
 
 
 def end_menu():
     '''
     present user with menu options at the end of game
     '''
-    print( f"""{Fore.WHITE}
+    print(f"""{Fore.WHITE}
         A - PLAY AGAIN
         B - LEADERBOARD
         C - INSTRUCTIONS
         D - EXIT GAME
         """)
-    while True:        
-        user_choice = input(Fore.WHITE + 'Please choose an option from the list above: \n').upper()
+    while True:
+        user_choice = input(Fore.WHITE + 'Please choose an option from '
+                            'the list above: \n').upper()
         if user_choice == 'A':
             hangman()
         elif user_choice == 'B':
             display_leaderboard()
         elif user_choice == 'C':
-            display_instructions()  
+            display_instructions()
         elif user_choice == 'D':
-            print(f'Thanks for playing')
+            print(f'Thanks for playing {USERNAME}')
             exit()
         else:
-            print(f'{Fore.RED} That is not a valid option. Please try again.\n')  
+            print(Fore.RED + 'That is not a valid option. Please try again.\n')
+
 
 def hangman():
     '''
@@ -146,12 +154,12 @@ def hangman():
     '''
     print("\n")
     print(f"Great! Let's get started {USERNAME}!")
-    type(f"S E L E C T I N G   W O R D . . . ."'\n')
-    type(f"L E T 'S   G O!!"'\n')
-    
-    selected_word = get_word(WORD_OPTIONS) # use get_word function to randomly select a word from the words worksheet
-    word_letters = set(selected_word) # identify letters in the word
-    alphabet = set(string.ascii_uppercase) 
+    type("S E L E C T I N G   W O R D . . . ."'\n')
+    type("L E T 'S   G O!!"'\n')
+    # use get_word function to randomly select a word from the words worksheet
+    selected_word = get_word(WORD_OPTIONS)
+    word_letters = set(selected_word)  # identify letters in the word
+    alphabet = set(string.ascii_uppercase)
     guessed_letters = set()
     wrong = 0
     lives = 7
@@ -160,34 +168,32 @@ def hangman():
     while len(word_letters) > 0 and lives > 0:
         print(graphics[wrong])
         print(Fore.WHITE + 'You have', lives, 'lives left\n')
-        print(Fore.WHITE + 'You have used these letters: ', ' '.join(guessed_letters),'\n')
+        print(Fore.WHITE + 'You have used these letters: ', ', '
+              .join(guessed_letters), '\n')
         print(Fore.WHITE + 'You have: ', points, 'points.\n')
 
-        word_list = [letter if letter in guessed_letters else '_' for letter in selected_word]
-        print('Selected word: ', ' '.join(word_list), )
+        word_list = [letter if letter in guessed_letters
+                     else '_' for letter in selected_word]
+        print('Selected word: ', ' '.join(word_list))
         print("====================================\n")
-
-        user_guess = input(Fore.WHITE + 'Pick a letter: \n').upper() # get user to pick a letter
-        
+        # get user to pick a letter
+        user_guess = input(Fore.WHITE + 'Pick a letter: \n').upper()
         if user_guess in alphabet - guessed_letters:
             guessed_letters.add(user_guess)
             if user_guess in word_letters:
                 word_letters.remove(user_guess)
-                
                 print(Fore.GREEN + user_guess, ' is in the word.')
-
             else:
                 lives = lives - 1
                 wrong = wrong + 1
                 points = points - 10
-                print(Fore.RED + user_guess,' is NOT in the word.')
-
+                print(Fore.RED + user_guess, ' is NOT in the word.')
         elif user_guess in guessed_letters:
-            print(Fore.RED + 'You have already picked', user_guess, '. Please try again.\n')
-
+            print(Fore.RED + 'You have already picked', user_guess,
+                  '. Please try again.\n')
         else:
-            print(Fore.RED + 'Invalid choice. Please choose a letter of the alphabet.\n')
-
+            print(Fore.RED + 'Invalid choice. Please choose a'
+                  'letter of the alphabet.\n')
     if lives == 0:
         print(graphics[7])
         print(graphics[9])
@@ -197,7 +203,8 @@ def hangman():
         update_leaderboard()
         end_menu()
     else:
-        print(Fore.GREEN + 'Congratulations! You guessed the word was ', selected_word, 'You scored', points, 'points!\n')
+        print(Fore.GREEN + 'Congratulations! You guessed the word was ',
+              selected_word, 'You scored', points, 'points!\n')
         print(graphics[8])
         FINAL_POINTS = str(points)
         update_leaderboard()
@@ -209,16 +216,14 @@ def display_instructions():
     display instructions from menu items
     '''
     print(game_rules)
-    while True:        
+    while True:
         go_back = input(Fore.WHITE + 'Please hit B to go back \n').upper()
         if go_back == 'B':
             game_menu()
         else:
-            print(f'{Fore.RED} That is not a valid option. Please try again.\n')  
+            print(f'{Fore.RED} That is not a valid option.'
+                  'Please try again.\n')
+
 
 welcome_msg()
 game_menu()
-
-
-
-
